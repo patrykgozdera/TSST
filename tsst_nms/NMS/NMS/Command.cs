16c7d157b2ebce5ef13d179a3fe5b26e58d32cbb
patrykgozdera <patryk.gozdera@gmail.com>
2017-11-26 00:10:59 +0100

    0.0.0
    Czyta z pliku, wysyła do agenta dane potrzebne do FIBa, jeszcze trzeba będzie ogarnąć usuwanie i dodawanie w trakcie działania całego projektu.

---
+++ tsst_nms/NMS/NMS/Command.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMS
+{
+    [Serializable]
+    public class Command
+    {
+        public string agentInterface;
+        public int agentPort;
+        public int inPort;
+        public int inLabel;
+        public int outPort;
+        public int outLabel;
+
+        public Command(string ai, int ap, int ip, int il, int op, int ol)
+        {
+            this.agentInterface = ai;
+            this.agentPort = ap;
+            this.inPort = ip;
+            this.inLabel = il;
+            this.outPort = op;
+            this.outLabel = ol;
+        }
+
+        public Command()
+        { }
+    }
+}
---
+++ tsst_nms/NMS/NMS/Config.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace NMS
+{
+    class Config
+    {
+        public static String getProperty(String key)
+        {
+            String value = ConfigurationManager.AppSettings[key];
+            return value;
+        }
+        public static int getIntegerProperty(String key)
+        {
+            String value = ConfigurationManager.AppSettings[key];
+            int intValue = int.Parse(value);
+            return intValue;
+        }
+    }
+}

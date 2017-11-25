abd75939813a146ea17d6027eef6cd7849a586c7
patrykgozdera <patryk.gozdera@gmail.com>
2017-11-26 00:29:05 +0100

    ###
    Agent gotowy do odbioru

---
+++ Agent/Agent/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agent
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RouterAgent router = new RouterAgent();
+            router.Listen();
+        }
+    }
+}

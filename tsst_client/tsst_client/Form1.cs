147d4f6be2476d2480820c26211cc69772407b31
maciekkobierecki <maciekkobierecki@gmail.com>
2017-11-28 22:00:58 +0100

    0.0.6

--- tsst_client/tsst_client/Form1.cs
+++ tsst_client/tsst_client/Form1.cs
@@ -25,7 +25,7 @@ namespace tsst_client
 
         public Form1()
         {
-            InitializeComponent();             
+            InitializeComponent();
         }
 
         private void Connect()                  //Połączenie
@@ -33,13 +33,13 @@ namespace tsst_client
             output_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPAddress ipAdd = IPAddress.Parse("127.0.0.1");
             IPEndPoint remoteEP = new IPEndPoint(ipAdd, Int32.Parse(textBox1.Text)); //Int32.Parse(ConfigurationManager.AppSettings["output_port"]));
-            output_socket.Connect(remoteEP);    
-        
-        }    
+            output_socket.Connect(remoteEP);
+
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+
             Connect();
             if (!backgroundWorker1.IsBusy)
             {
@@ -49,16 +49,16 @@ namespace tsst_client
 
         private void send_Click(object sender, EventArgs e)
         {
-            SendMessage(messageOut);            
+            SendMessage(messageOut);
         }
 
         private void SendMessage(Message packet)              //Wysyłanie. Wysyła określoną liczbę pakietów co określony odstęp czasu. 
         {
             string random_meassage;
             Thread thread;
-            thread = new Thread(async () =>           
+            thread = new Thread(async () =>
             {
-                for (int i=1; i <= Int32.Parse(nb_of_m_tb.Text); i++)
+                for (int i = 1; i <= Int32.Parse(nb_of_m_tb.Text); i++)
                 {
                     random_meassage = message_tb.Text + RandomString();
                     packet = new Message(random_meassage, Int32.Parse(textBox2.Text), Int32.Parse(textBox1.Text), GetTimeStamp(), "I1");
@@ -72,14 +72,14 @@ namespace tsst_client
                         output_socket.Send(GetSerializedMessage(packet));
                         logs_list.Invoke(new Action(delegate ()
                         {
-                            logs_list.Items.Add(i + ": " + packet.s + " | " + packet.output_port + " | " + packet.timestamp);
+                            logs_list.Items.Add(i + ": " + messageSize + "|" + packet.s + " | " + packet.output_port + " | " + packet.timestamp);
                             logs_list.SelectedIndex = logs_list.Items.Count - 1;
-                        }));           
+                        }));
 
                     }
 
                     //Thread.Sleep(Int32.Parse(delay_tb.Text));
-                    await Task.Delay(Int32.Parse(delay_tb.Text));                    
+                    await Task.Delay(Int32.Parse(delay_tb.Text));
                 }
             }
             );
@@ -103,7 +103,7 @@ namespace tsst_client
                 {
                     logs_list.Items.Add(": " + sm.s + " | " + sm.output_port + " | " + sm.timestamp);
                     logs_list.SelectedIndex = logs_list.Items.Count - 1;
-                }));                
+                }));
             }
             );
             tr.Start();
@@ -114,8 +114,7 @@ namespace tsst_client
             inputSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPAddress ipAdd = IPAddress.Parse("127.0.0.1");
             IPEndPoint remoteEP = new IPEndPoint(ipAdd, Int32.Parse(textBox2.Text)); //Int32.Parse(ConfigurationManager.AppSettings["input_port"]));
-            inputSocket.Bind(remoteEP);
-            inputSocket.Connect();
+            inputSocket.Connect(remoteEP);
             int i = 1;
 
             while (true)
@@ -123,31 +122,31 @@ namespace tsst_client
                 //inputSocket.Listen(0);                
                 //foreignSocket = inputSocket.Accept();
                 byte[] objectSize = new byte[4];
-                foreignSocket.Receive(objectSize, 0, 4, SocketFlags.None);
+                inputSocket.Receive(objectSize, 0, 4, SocketFlags.None);
                 int messageSize = BitConverter.ToInt32(objectSize, 0);
 
                 byte[] bytes = new byte[messageSize];
-                int readByte = foreignSocket.Receive(bytes);
+                int readByte = inputSocket.Receive(bytes, 0, messageSize, SocketFlags.None);
                 Thread t;
                 t = new Thread(() =>
                 {
-                    messageIn = GetDeserializedMessage(bytes);                    
+                    messageIn = GetDeserializedMessage(bytes);
                     receive_logs_list.Invoke(new Action(delegate ()
                     {
-                        receive_logs_list.Items.Add(i + ": " + messageIn.s + " | " + messageIn.output_port + " | " + messageIn.timestamp);
+                        receive_logs_list.Items.Add(i + ": " + messageSize + "|" + messageIn.s + " | " + messageIn.output_port + " | " + messageIn.timestamp);
                         receive_logs_list.SelectedIndex = receive_logs_list.Items.Count - 1;
                     }));
                     //SendSingleMessage(messageIn);
-                    i++;                    
+                    i++;
                 }
                 );
-                t.Start();               
-                             
-            }            
-        }           
+                t.Start();
+
+            }
+        }
 
         private byte[] GetSerializedMessage(Message mes)    //Serializacja bajtowa
-        {                        
+        {
             BinaryFormatter bf = new BinaryFormatter();
             MemoryStream ms = new MemoryStream();
             bf.Serialize(ms, mes);
@@ -155,15 +154,15 @@ namespace tsst_client
         }
 
         private Message GetDeserializedMessage(byte[] b)        //Deserializacja
-        {         
+        {
             Message m = new Message();
             MemoryStream memStream = new MemoryStream();
             BinaryFormatter binForm = new BinaryFormatter();
             memStream.Write(b, 0, b.Length);
             memStream.Seek(0, SeekOrigin.Begin);
             m = (Message)binForm.Deserialize(memStream);
-            return m;                               
-        }      
+            return m;
+        }
 
 
 
@@ -171,10 +170,10 @@ namespace tsst_client
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)         //Backgrounworker - w tle nasłuchuje
         {
             try
-            {                
+            {
                 Listen();
             }
-            catch(IOException)
+            catch (IOException)
             {
                 logs_list.Invoke(new Action(delegate ()
                 {
@@ -187,15 +186,15 @@ namespace tsst_client
         {
             if (e.Cancelled)
             {
-                MessageBox.Show("Processing cancelled", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information); 
+                MessageBox.Show("Processing cancelled", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else if (e.Error != null)
             {
-                MessageBox.Show(e.Error.Message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information); 
+                MessageBox.Show(e.Error.Message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
-                MessageBox.Show(e.Result.ToString(), "Information", MessageBoxButtons.OK, MessageBoxIcon.Information); 
+                MessageBox.Show(e.Result.ToString(), "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
@@ -206,26 +205,26 @@ namespace tsst_client
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, string_length).Select(s => s[random.Next(s.Length)]).ToArray());
         }
-        
+
         private string GetTimeStamp()
         {
             DateTime dateTime = DateTime.Now;
             return dateTime.ToString("yyyy-MM-dd HH:mm:ss.ff");
         }
-       
-  
+
+
         //////////////////////////////////////////////////////////////////
-       
+
 
 
 
         private string GetSerializeXmlObject()      //Serializacja do XMLa
         {
             XmlSerializer xsSubmit = new XmlSerializer(typeof(Message));
-            var subReq = new Message(message_tb.Text, 123, Int32.Parse(ConfigurationManager.AppSettings["client_port"]), GetTimeStamp(),"I1");
+            var subReq = new Message(message_tb.Text, 123, Int32.Parse(ConfigurationManager.AppSettings["client_port"]), GetTimeStamp(), "I1");
             //Message mes = new Message();
             //mes.s = message_tb.Text;
-             
+
             var sw = new StringWriter();
             XmlTextWriter writer = new XmlTextWriter(sw);
             xsSubmit.Serialize(writer, subReq);
